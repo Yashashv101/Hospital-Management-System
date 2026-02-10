f8a136ba847d5a59332eae4e4cdcfa552684fcc1
Yashashv <yashashvsinghxia1397@gmail.com>
2026-02-10 20:34:23 +0530

    Authenticated pages with roles

--- src/main/java/com/codingshuttle/youtube/hospitalManagement/config/AppConfig.java
+++ src/main/java/com/codingshuttle/youtube/hospitalManagement/config/AppConfig.java
@@ -1,8 +1,11 @@
 package com.codingshuttle.youtube.hospitalManagement.config;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.BeanRegistrarDslMarker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +13,10 @@ public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/com/codingshuttle/youtube/hospitalManagement/entity/type/User.java
@@ -0,0 +1,26 @@
+package com.codingshuttle.youtube.hospitalManagement.entity.type;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Data
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique=true)
+    private String username;
+    private String password;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+}
